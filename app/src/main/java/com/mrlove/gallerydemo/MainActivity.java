563736ca0359527fec0139a9d039aa4031267435
Mrlove133481 <1334819077@qq.com>
2020-04-07 20:00:29 +0800

    swipeRefreshLayout刷新正常使用

--- app/src/main/java/com/mrlove/gallerydemo/MainActivity.java
+++ app/src/main/java/com/mrlove/gallerydemo/MainActivity.java
@@ -31,24 +31,24 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "Mrlove-MainActivity";
-    //ImageView imageView = findViewById(R.id.imageView2);
-    TextView textView = findViewById(R.id.textView2);
-    SwipeRefreshLayout swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
+    ImageView imageView;
+    TextView textView;
+    SwipeRefreshLayout swipeRefreshLayout;
     String url1 = "https://www.baidu.com/img/superlogo_c4d7df0a003d3db9b65e9ef0fe6da1ec.png";
     String url2 = "https://img.alicdn.com/tfs/TB1_uT8a5ERMeJjSspiXXbZLFXa-143-59.png";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        imageView = findViewById(R.id.imageView);
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
         //1.创建一个volley队列
         RequestQueue queue = Volley.newRequestQueue(this);
-
+        //2.设置swipeRefreshLayout监听器，监听下拉刷新，执行相应的下拉操作
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                textView.setText("hahahahah");
-                //loadImage();
+                loadImage();
             }
         });
     }
@@ -62,21 +62,25 @@ public class MainActivity extends AppCompatActivity {
                 .load(url)
                 //图片预设图片
                 .placeholder(R.drawable.ic_launcher_background)
-                //监听器
+                //监听器，当数据成功加载或者失败时，都停止下来刷新转圈。
                 .listener(new RequestListener<Drawable>() {
                     @Override
                     public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
-
+                        if(swipeRefreshLayout.isRefreshing()){
+                            swipeRefreshLayout.setRefreshing(false);
+                        }
                         return false;
                     }
 
                     @Override
                     public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
-
+                        if(swipeRefreshLayout.isRefreshing()){
+                            swipeRefreshLayout.setRefreshing(false);
+                        }
                         return false;
                     }
-                });
+                })
                 //加载图片控件
-              //  .into(imageView);
+                .into(imageView);
     }
 }
