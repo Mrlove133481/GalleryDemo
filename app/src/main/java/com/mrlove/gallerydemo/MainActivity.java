82d9f9d745ef04f3196f0cc098a8988e722f059e
Mrlove133481 <1334819077@qq.com>
2020-04-07 16:14:37 +0800

    Volley原生加载图片实现

--- app/src/main/java/com/mrlove/gallerydemo/MainActivity.java
+++ app/src/main/java/com/mrlove/gallerydemo/MainActivity.java
@@ -2,15 +2,19 @@ package com.mrlove.gallerydemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.LruCache;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -20,31 +24,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView textView = findViewById(R.id.textView);
-        String url = "https://www.baidu.com";
+        final ImageView imageView = findViewById(R.id.imageView);
+        String url = "https://www.baidu.com/img/superlogo_c4d7df0a003d3db9b65e9ef0fe6da1ec.png";
         //1.创建一个volley队列
         RequestQueue queue = Volley.newRequestQueue(this);
-        //2.创建一个请求,请求有三个参数:
-            //Method:请求的方式
-            //Response.Listener:正确响应处理操作
-            //Response.ErrorListener:错误响应处理操作
-        StringRequest stringRequest = new StringRequest(
-                StringRequest.Method.GET,
-                url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        textView.setText(response);
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e(TAG,"onErrorResponse",error);
-                    }
-                }
-        );
-        //把请求添加到队列中处理,Volley执行的是异步操作.
-        queue.add(stringRequest);
+        //2.创建imageLoader对象,管理图片 ,imageLoader对象有两个参数:
+            //RequestQueue:请求的队列
+            //ImageLoader.ImageCache: 缓存设置
+        ImageLoader imageLoader = new ImageLoader(queue, new ImageLoader.ImageCache() {
+            //3.Lru–>(Least recent used)最少最近使用算法 设置图片的缓冲对象为50个.
+            private LruCache<String,Bitmap> cache = new LruCache<>(50);
+            @Override
+            public Bitmap getBitmap(String url) {
+                //得到缓冲图片地址
+                return cache.get(url);
+            }
+            @Override
+            public void putBitmap(String url, Bitmap bitmap) {
+                //放进缓存,图片的URL地址和图片
+                cache.put(url,bitmap);
+            }
+        });
+        //4.通过imageLoader.get()方法设置图片,get方法有两个参数:
+            //url:图片地址
+            //ImageLoader.ImageListener 监听对象
+                    //Response.Listener:正确响应处理操作
+                    //Response.ErrorListener:错误响应处理操作
+        imageLoader.get(url, new ImageLoader.ImageListener() {
+            @Override
+            public void onResponse(ImageLoader.ImageContainer response, boolean isImmediate) {
+                imageView.setImageBitmap(response.getBitmap());
+            }
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "onErrorResponse: ",error );
+            }
+        });
+
     }
 }
