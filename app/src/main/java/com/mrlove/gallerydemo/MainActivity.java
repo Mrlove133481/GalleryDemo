00847dd044ee6d3af901ee0920819efb99f3d08b
Mrlove133481 <1334819077@qq.com>
2020-04-07 14:50:47 +0800

    Volley的简单使用

--- app/src/main/java/com/mrlove/gallerydemo/MainActivity.java
+++ app/src/main/java/com/mrlove/gallerydemo/MainActivity.java
@@ -2,13 +2,46 @@ package com.mrlove.gallerydemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "Mrlove-MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView textView = findViewById(R.id.textView);
+        String url = "https://www.jd.com";
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(
+                StringRequest.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        textView.setText(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e(TAG,"onErrorResponse",error);
+                    }
+                }
+
+        );
+        queue.add(stringRequest);
+
+
     }
 }
